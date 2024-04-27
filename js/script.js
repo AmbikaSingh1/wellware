e9f26be27911d157f9712932f63eabb435068cd8
AmbikaSingh1 <ambikathakur013@gmail.com>
2024-04-27 20:02:31 +0530

    firstcommit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $(".innerprod").toggle();
+    $(".m .toggle").click(function(){
+        $(".innerprod").toggle();
+        
+    });
+});
+$(document).ready(function(){
+    $(".nav-mob").click(function(){
+        $(".hold").toggle();
+        $(".mobile-nav-toggle1").toggle().css("margin-top","1em");
+        $(".nav-mob").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $(".mobile-nav-toggle1").click(function(){
+        $(".hold").toggle();
+        $(".mobile-nav-toggle1").toggle().css("margin-top","1em");
+        $(".nav-mob").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $(".togglee").click(function(){
+        $(".inner").toggle();
+    });
+});
+    
+
